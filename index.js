57e7a4183c6e2faba8ea3d5370207024a324cad8
Ryan Erricson <ryanerricson@gmail.com>
2021-05-12 17:25:42 -0400

    commit tree 2fe9f6901480db3e003ab447537bfce81814a86f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice(array.length - 2, array.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (int) => {
+    
+    return function faremultiplier(fare) {
+        return fare * int
+    }
+    
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2
+}
+
+const fareTripler = (fare) => {
+    return fare * 3
+}
+
+const selectDifferentDrivers = (array, v) => {
+    if (v === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(array)
+    } else {
+        return returnLastTwoDrivers(array)
+    }
+}
